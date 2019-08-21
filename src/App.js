b3765279be42a1e6394878ddab74c618a3d93672
Jun-Sik Choi <leichoi91@gmail.com>
2019-08-21 15:19:30 +0700

    # Typechecking with proptypes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
 import React from 'react';
-
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+import propTypes from "prop-types";
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4
   },
   {
     id: 2,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 3.4
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 5
   },
   {
     id: 4,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 3.5
   },
   {
     id: 5,
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.3
   }
 ];
 
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I love {name}</h2>
+      <h3>rating : {rating} / 5.0</h3>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: propTypes.string.isRequired,
+  picture: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired
+}
+
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+}
+
 function App() {
   return (
     <div>
