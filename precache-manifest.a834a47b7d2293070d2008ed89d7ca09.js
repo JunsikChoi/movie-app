a5ecab0060d1af5fc272397a31fd7c194c7520f7
Jun-Sik Choi <leichoi91@gmail.com>
2019-08-22 18:30:13 +0700

    Updates

---
+++ precache-manifest.a834a47b7d2293070d2008ed89d7ca09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8b572c91c02df54bf1816ee320eb86",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "272fa0b1105f3ad69ac8",
+    "url": "/movie-app/static/css/main.6d52ebcd.chunk.css"
+  },
+  {
+    "revision": "35553d95505140ba87a3",
+    "url": "/movie-app/static/js/2.e5148252.chunk.js"
+  },
+  {
+    "revision": "272fa0b1105f3ad69ac8",
+    "url": "/movie-app/static/js/main.c057e35a.chunk.js"
+  },
+  {
+    "revision": "c3c6c44c5dca62de27bd",
+    "url": "/movie-app/static/js/runtime~main.8571d2f1.js"
+  }
+]);
